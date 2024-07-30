5501621b6124ffb34a303f8ea84fb1fe94f5f969
ShionSato <208449@st.takushoku-u.ac.jp>
2024-07-30 17:56:18 +0900

    windowの削除

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
-'use client'
-import { useEffect } from 'react';
+import ErrorBoundary from './components/ErrorBoundary';
 import styles from "./page.module.css";
 import Header from "./components/Layout/Header";
 import Sidebar from "./components/Layout/Sidebar";
@@ -7,44 +6,22 @@ import List from "./components/List/list";
 import Status from "./components/status/status";
 
 export default function Home() {
-  useEffect(() => {
-    const handleError = (event) => {
-      if (event.message.includes('Hydration failed')) {
-        event.stopImmediatePropagation();
-      }
-    };
-
-    const handleUnhandledRejection = (event) => {
-      if (event.reason.message.includes('Hydration failed')) {
-        event.stopImmediatePropagation();
-      }
-    };
-
-    window.addEventListener('error', handleError);
-    window.addEventListener('unhandledrejection', handleUnhandledRejection);
-
-    return () => {
-      window.removeEventListener('error', handleError);
-      window.removeEventListener('unhandledrejection', handleUnhandledRejection);
-    };
-  }, []);
-
   return (
-    <>
-    <main className={styles.main}>
-      <Header />
-      <div className={styles.content}>
-        <div>
-          <Sidebar />
+    <ErrorBoundary>
+      <main className={styles.main}>
+        <Header />
+        <div className={styles.content}>
+          <div>
+            <Sidebar />
+          </div>
+          <div className={styles.List}>
+            <List />
+          </div>
+          <div className={styles.detail}>
+            <Status />
+          </div>
         </div>
-        <div className={styles.List}>
-          <List />
-        </div>
-        <div className={styles.detail}>
-          <Status />
-        </div>
-      </div>
-    </main>
-    </>
-  )
+      </main>
+    </ErrorBoundary>
+  );
 }
