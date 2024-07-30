4ab4f3d9dd857570ac287fa4ce839102c4046b82
ShionSato <208449@st.takushoku-u.ac.jp>
2024-07-30 17:59:20 +0900

    元通り

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-import ErrorBoundary from './components/ErrorBoundary';
+'use client'
+import { useEffect } from 'react';
 import styles from "./page.module.css";
 import Header from "./components/Layout/Header";
 import Sidebar from "./components/Layout/Sidebar";
@@ -6,22 +7,44 @@ import List from "./components/List/list";
 import Status from "./components/status/status";
 
 export default function Home() {
+  useEffect(() => {
+    const handleError = (event) => {
+      if (event.message.includes('Hydration failed')) {
+        event.stopImmediatePropagation();
+      }
+    };
+
+    const handleUnhandledRejection = (event) => {
+      if (event.reason.message.includes('Hydration failed')) {
+        event.stopImmediatePropagation();
+      }
+    };
+
+    window.addEventListener('error', handleError);
+    window.addEventListener('unhandledrejection', handleUnhandledRejection);
+
+    return () => {
+      window.removeEventListener('error', handleError);
+      window.removeEventListener('unhandledrejection', handleUnhandledRejection);
+    };
+  }, []);
+
   return (
-    <ErrorBoundary>
-      <main className={styles.main}>
-        <Header />
-        <div className={styles.content}>
-          <div>
-            <Sidebar />
-          </div>
-          <div className={styles.List}>
-            <List />
-          </div>
-          <div className={styles.detail}>
-            <Status />
-          </div>
+    <>
+    <main className={styles.main}>
+      <Header />
+      <div className={styles.content}>
+        <div>
+          <Sidebar />
         </div>
-      </main>
-    </ErrorBoundary>
-  );
+        <div className={styles.List}>
+          <List />
+        </div>
+        <div className={styles.detail}>
+          <Status />
+        </div>
+      </div>
+    </main>
+    </>
+  )
 }
